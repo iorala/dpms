c4ec8ded5c9274b42b430b4212620ca58eef41dd
iorala <andreas.laroi@gmail.com>
2017-12-21 22:02:39 +0100

    Add Website content

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/* initialize fancybox */
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
+		autoCenter : true,
+		type : 'image',
+		closeBtn : true
+	});
+});
